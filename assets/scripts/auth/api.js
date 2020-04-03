7c6e19ebe28fc899a190ac3f8d820d0de16d9546
nicksolie <nick.solie1@gmail.com>
2020-04-03 10:45:27 -0400

    Auth functions work
    All auth buttons
    Added message to index.html

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const config = require('../config.js')
+const store = require('../store')
+
+const signUp = function (data) {
+  console.log(data)
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  console.log(data)
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: data
+  })
+}
+
+const changePassword = function (data) {
+  console.log(data)
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: data
+  })
+}
+
+const signOut = function (data) {
+  console.log(data)
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,87 @@
+'use strict'
+
+'use strict'
+const store = require('../store')
+
+const signUpSuccess = function () {
+  $('#message').text('You have successfully signed up!')
+  $('#message').removeClass()
+  $('#message').addClass('sucess')
+  document.getElementById('sign-up').reset()
+  $('#sign-in').closest('form').find('input[type=password], textarea').val('')
+  $('#sign-in').closest('form').find('input[type=text], textarea').val('')
+}
+
+const signUpFailure = function () {
+  $('#message').text('A sign up error has occured.')
+  $('#message').removeClass()
+  $('#message').addClass('failed')
+  document.getElementById('sign-up').reset()
+  $('#sign-in').closest('form').find('input[type=password], textarea').val('')
+  $('#sign-in').closest('form').find('input[type=text], textarea').val('')
+}
+
+const signInSuccess = function (data) {
+  $('#message').text('Signed in successfully.')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  $('#sign-up').addClass('hidden')
+  $('#sign-out').removeClass('hidden')
+  $('#change-password').removeClass('hidden')
+  $('#sign-in').addClass('hidden')
+  document.getElementById('sign-in').reset()
+  $('#sign-up').closest('form').find('input[type=password], textarea').val('')
+  $('#sign-up').closest('form').find('input[type=text], textarea').val('')
+  store.user = data.user
+}
+
+const signInFailure = function () {
+  $('#message').text('Error, please try again.')
+  $('#message').removeClass()
+  $('#message').addClass('failed')
+  document.getElementById('sign-in').reset()
+  $('#sign-up').closest('form').find('input[type=password], textarea').val('')
+  $('#sign-up').closest('form').find('input[type=text], textarea').val('')
+}
+
+const changePasswordSuccess = function () {
+  $('#message').text('Changed password successfully')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  document.getElementById('change-password').reset()
+}
+
+const changePasswordFailure = function () {
+  $('#message').text('Change Password error')
+  $('#message').removeClass()
+  $('#message').addClass('failed')
+  document.getElementById('change-password').reset()
+}
+
+const signOutSuccess = function () {
+  $('#message').text('Sign out successfully.')
+  $('#message').removeClass()
+  $('#message').addClass('success')
+  $('#sign-in').removeClass('hidden')
+  $('#sign-out').addClass('hidden')
+  $('#sign-up').removeClass('hidden')
+  document.getElementById('change-password').reset()
+}
+
+const signOutFailure = function () {
+  $('#message').text('Change Password error.')
+  $('#message').removeClass()
+  $('#message').addClass('failed')
+  document.getElementById('change-password').reset()
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
